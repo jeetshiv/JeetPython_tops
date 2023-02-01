f3b7566a8b6fd69ff04f9d9affd14f9371815a49
jeetshiv <jjeet200@gmail.com>
2023-02-01 04:17:49 -0800

    Update tkinterdemo.py

--- classwork/tkinterdemo.py
+++ classwork/tkinterdemo.py
@@ -1,5 +1,42 @@
 from tkinter import *
 import tkinter.messagebox as msg
+import mysql.connector 
+
+def create_conn():
+    return mysql.connector.connect(
+        host="localhost",
+        username="root",
+        password="",
+        database="python")
+print(create_conn())
+
+def insert_data():
+    if e_fname.get()=="" or e_lname.get()=="" or e_email.get()=="" or e_mobile.get()=="":
+        msg.showinfo("insert status","All fields mandatory")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="insert into employee(fname,lname,email,mobile) values(%s,%s,%s,%s)"
+        args=(e_fname.get(),e_lname.get(),e_email.get(),e_mobile.get())
+        cursor.execute(query,args)
+        conn.commit()
+        conn.close()
+        msg.showinfo("insert status","Data inserted sucessfully")
+        e_fname.delete(0,"end")
+        e_lname.delete(0,"end")
+        e_email.delete(0,"end")
+        e_mobile.delete(0,"end")
+def search_data():
+    print("search clicked")
+    
+def update_data():
+    print("update clicked")
+    
+def delete_data():
+    print("delete clicked")
+
+
+
 
 root =  Tk()
 root.geometry("500x500")
@@ -22,7 +59,6 @@ l_mobile=Label(root,text="Mobile")
 l_mobile.place(x=50,y=250)
 
 
-
 e_id = Entry(root)
 e_id.place(x=150,y=50)
 
@@ -30,22 +66,24 @@ e_fname=Entry(root)
 e_fname.place(x=150,y=100)
 
 e_lname=Entry(root)
-e_lname.place(x=150,y=200)
+e_lname.place(x=150,y=150)
+
+e_email=Entry(root)
+e_email.place(x=150,y=200)
 
 e_mobile=Entry(root)
 e_mobile.place(x=150,y=250)
 
 
-insert=Button(root,text="Insert")
+insert=Button(root,text="Insert",bg="black",fg="white",font=("Arial,10"),command=insert_data)
 insert.place(x=50,y=300)
 
-search=Button(root,text="Search")
+search=Button(root,text="Search",bg="black",fg="white",font=("Arial,10"),command=search_data)
 search.place(x=120,y=300)
 
-update=Button(root,text="Update")
+update=Button(root,text="Update",bg="black",fg="white",font=("Arial,10"),command=update_data)
 update.place(x=190,y=300)
 
-delete=Button(root,text="Delete")
+delete=Button(root,text="Delete",bg="black",fg="white",font=("Arial,10"),command=delete_data)
 delete.place(x=260,y=300)
-
 
